1ba1b44fb21b33c02fd6473a7236ed421095d6f2
aldemirali93@gmail.com <aldemirali93@gmail.com>
2018-01-09 16:33:05 +0300

    -

---
+++ src/app/components/question/add/question.add.component.ts
@@ -0,0 +1,35 @@
+﻿import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CurriculumService } from '../../../shared/services/curriculum.service';
+import { Curriculum } from '../../../shared/models/curriculum.model';
+import { MatDialogRef, MatSnackBar } from '@angular/material';
+
+@Component({
+    templateUrl: './curriculum.add.component.html'
+})
+export class CurriculumAddComponent {
+    public form = new FormGroup({
+        name: new FormControl('', Validators.required),
+        isActive: new FormControl(Boolean, Validators.required)
+    });
+
+    constructor(
+        private curriculumService: CurriculumService,
+        public dialogRef: MatDialogRef<CurriculumAddComponent>,
+        public snackBar: MatSnackBar) { }
+
+    onSubmit(event: any) {
+        if (!this.form.valid) {
+            return false;
+        }
+
+        this.curriculumService
+            .Insert(new Curriculum(this.form.controls.name.value, this.form.controls.isActive.value))
+            .subscribe(isSuccess => {
+                this.snackBar.open(isSuccess.message, '', {
+                    duration: 3000,
+                });
+                this.dialogRef.close(isSuccess.model);
+            });
+    }
+}
---
+++ src/app/components/question/update/question.update.component.ts
@@ -0,0 +1,40 @@
+﻿import { Component, Inject } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CurriculumService } from '../../../shared/services/curriculum.service';
+import { Curriculum } from '../../../shared/models/curriculum.model';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { MatSnackBar } from '@angular/material';
+
+@Component({
+    templateUrl: './curriculum.update.component.html'
+})
+export class CurriculumUpdateComponent {
+    public form = new FormGroup({
+        name: new FormControl('', Validators.required),
+        isActive: new FormControl(Boolean, Validators.required)
+    });
+
+    constructor(
+        private curriculumService: CurriculumService,
+        public dialogRef: MatDialogRef<CurriculumUpdateComponent>,
+        public snackBar: MatSnackBar,
+        @Inject(MAT_DIALOG_DATA) public params: any) {
+        this.form.controls.name.setValue(params.name);
+        this.form.controls.isActive.setValue(params.isActive);
+    }
+
+    onSubmit(event: any) {
+        if (!this.form.valid) {
+            return false;
+        }
+
+        this.curriculumService
+            .Update(new Curriculum(this.form.controls.name.value, this.form.controls.isActive.value, this.params._id))
+            .subscribe(isSuccess => {
+                this.snackBar.open(isSuccess.message, '', {
+                    duration: 3000,
+                });
+                this.dialogRef.close(isSuccess.model);
+            });
+    }
+}
