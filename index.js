a70281798598a37f7aa093b9ffb6781167e22271
alialdemir <aldemirali93@gmail.com>
2018-02-01 02:20:08 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+    .use(express.static(path.join(__dirname, 'dist')))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.sendfile('dist/index.html'))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`))
